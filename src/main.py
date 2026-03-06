00ece7f2ddf0055f8b0805f1bb5b647845c39606
diego <officialbykoan@gmail.com>
2026-03-06 10:58:39 +0100

    Feat: Upload func in other route for more clean interface

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from config import Config
 from database.db import create_user_db
 from routes.auth_routes import auth_bp
 from routes.music_routes import music_bp
+from routes.upload_routes import upload_bp
 from dotenv import load_dotenv
 import os
 
@@ -18,6 +19,7 @@ def create_app():
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(music_bp)
+    app.register_blueprint(upload_bp)
 
     return app
 
